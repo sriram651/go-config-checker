9230e5832ffad11b4fde786410c72a8bdea6d181
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-01 15:46:50 +0530

    feat: read the file and find the length of bytes

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	configPath := flag.String("config", "", "path to config file")
-	schemaPath := flag.String("schema", "", "path to schema file")
+	// schemaPath := flag.String("schema", "", "path to schema file")
 	flag.Parse()
 
-	fmt.Println("config:", *configPath)
-	fmt.Println("schema:", *schemaPath)
+	fileBytes, err := os.ReadFile(*configPath)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Byte length:", len(fileBytes))
 }
